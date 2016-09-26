b3ebf20a2abe9b529ef23d2a2c375032b33ebcf8
aleksei-g <lexa_prorock@mail.ru>
2016-09-26 22:37:07 +0300

    задача решена

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,23 @@
 import json
+import os.path
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r', encoding='utf_8') as f:
+        data = json.loads(f.read())
+    return data
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    pass
+    while True:
+        filepath = input('Enter file path: ')
+        if os.path.exists(filepath):
+            break
+        else:
+            print('File not found!')
+    data = load_data(filepath)
+    pretty_print_json(data)

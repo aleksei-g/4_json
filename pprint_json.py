b7a3d5c4400604a3198dd4b22fe4254435c10bc6
aleksei-g <lexa_prorock@mail.ru>
2016-09-29 22:46:09 +0300

    Добавлен функционал argparse

--- pprint_json.py
+++ pprint_json.py
@@ -1,23 +1,32 @@
 import json
 import os.path
+import argparse
 
 
 def load_data(filepath):
+    if not os.path.exists(filepath):
+        return None
     with open(filepath, 'r', encoding='utf_8') as f:
-        data = json.loads(f.read())
-    return data
+        return json.load(f)
 
 
 def pretty_print_json(data):
     print(json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False))
 
 
+def createParser():
+    parser = argparse.ArgumentParser(description='Чтение файлов  json \
+                                     в удобном формате')
+    parser.add_argument('-f', '--file', required=True, metavar='ФАЙЛ',
+                        help='Путь до файла в формате json.')
+    return parser
+
+
 if __name__ == '__main__':
-    while True:
-        filepath = input('Enter file path: ')
-        if os.path.exists(filepath):
-            break
-        else:
-            print('File not found!')
-    data = load_data(filepath)
-    pretty_print_json(data)
+    parser = createParser()
+    namespace = parser.parse_args()
+    data = load_data(namespace.file)
+    if data:
+        pretty_print_json(data)
+    else:
+        print('Файл не найден!')
